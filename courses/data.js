96f5901027c4ea62e73f9e004d377966bcfde499
Shivhari Shankar <shivhari.shankar@gmail.com>
2021-06-08 12:25:17 +0530

    Init commit

---
+++ courses/data.js
@@ -0,0 +1,68 @@
+let data = [
+  {
+    name:'Bonfida Serum DEX',
+    url:'https://www.youtube.com/channel/UCRQhZGXC0WK85YRXl7nGX0w',
+    description:'Bonfida DEX will give traders the best functionality and experience maintaining its place as the most used Serum DEX, including a builtin wallet, starter SOL airdrops, and more.',
+    image: 'bonfida.png',
+    tags:['web', 'token','bots','voting','advanced']
+  },
+  {
+    name: "DexLab",
+    url:"https://www.dexlab.space/",
+    description:
+      "Dexlab is a decentralized exchange where the best Solana projects mint and list their tokens.",
+    image: "dexlab.png",
+    tags: ["minting","web","analytics"],
+  },
+  {
+    name: "Cryptocurrencies.Ai DEX",
+    url: "https://dex.cryptocurrencies.ai/",
+    description: "One of the fastest DEXs in the world, built on the Serum and Solana technology. User-friendly DeFi experience with low fees.",
+    image: "dexccai.png",
+    tags: ["web","analytics","wallet","addressbook"],
+  },
+  {
+    name: "Serum Today",
+    url: "https://serum.today/",
+    description:"UI Clone of Project Serum Dex",
+    image: "serumtoday.png",
+    tags: ["web"],
+  },
+  {
+    name: "Serum Pro",
+    url: "https://serumpro.org/",
+    description:"UI Clone of Project Serum Dex",
+    image: "serumtoday.png",
+    tags: ["web"],
+  },
+  {
+    name: "Viralic DEX",
+    url:"https://dex.viralic.io/",
+    description: "A new Scarcity Mining and Staking DeFi paradigm for Viral adoption on Binance Smart Chain",
+    image: "viralic.png",
+    tags: ["web",],
+  },
+  {
+    name:"Project Serum DEX",
+    url: "https://dex.projectserum.com/",
+    description: "Project Serum's DEX",
+    image: "serumtoday.png",
+    tags: ["web-development", "Edureka"],
+  },
+  {
+    name: "Santosha Exchange",
+    url:
+      "https://exchange.santosha.digital/",
+    description: "Similar to Project serum's dex with additional OTC capabilities",
+    image: "santosha.png",
+    tags: ["web", "otc"],
+  },
+  {
+    name: "Serum Folkvang.io",
+    url: "https://serum-mirror.folkvang.io/",
+    description:
+      "UI Clone of Project Serum Dex",
+    image: "serumtoday.png",
+    tags: ["linux", "bash"],
+  },
+];
