1d9d4a09357330b703087f800d26ad225293e165
Shivhari Shankar <shivhari.shankar@gmail.com>
2021-06-12 13:00:08 +0530

    added 3 more dApps

--- courses/data.js
+++ courses/data.js
@@ -7,6 +7,13 @@ let data = [
     tags:['web', 'token','bots','voting','advanced']
   },
   {
+    name: "Coin98",
+    url:"https://www.dexlab.space/",
+    description:"The first in the world using a decentralized wallet with experiences of a centralized one",
+    image: "coin98.png",
+    tags: ["minting","mobile","analytics"],
+  },
+  {
     name: "DexLab",
     url:"https://www.dexlab.space/",
     description:
@@ -27,14 +34,13 @@ let data = [
     description: "Project Serum's DEX",
     image: "serumtoday.png",
     tags: ["web"],
-  },
+  },  
   {
-    name: "Ourord DEX",
-    url: "https://dex.ourord.com/",
-    description:
-      "UI Clone of Project Serum Dex with UI text in Spanish",
-    image: "ourord.png",
-    tags: ["web","spanish"],
+    name: "Open Serum",
+    url:"https://openserum.ch/",
+    description: "Simple to use Serum DEX with the option for Pool, Swap and insightful charts / metrics.",
+    image: "openserum.png",
+    tags: ["swap","web","pool","analytics"],
   },
   {
     name: "YMAX Dex",
@@ -59,12 +65,28 @@ let data = [
     tags: ["web", "otc"],
   },
   {
+    name: "Ourord DEX",
+    url: "https://dex.ourord.com/",
+    description:
+      "UI Clone of Project Serum Dex with UI text in Spanish",
+    image: "ourord.png",
+    tags: ["web","spanish"],
+  },
+  {
     name: "Serum Folkvang.io",
     url: "https://serum-mirror.folkvang.io/",
     description:
       "UI Clone of Project Serum Dex",
     image: "serumtoday.png",
-    tags: ["web"],
+    tags: ["web","swap","pool"],
+  },
+  {
+    name: "Luna DEX",
+    url: "https://serum-mirror.folkvang.io/",
+    description:
+      "UI Clone of Project Serum Dex",
+    image: "lunadex.png",
+    tags: ["web","swap","pool"],
   },
   {
     name: "TradeOnSerum DEX",
@@ -87,5 +109,19 @@ let data = [
     description:"UI Clone of Project Serum Dex",
     image: "serumtoday.png",
     tags: ["web"],
+  },
+  {
+    name: "Serum Trade",
+    url: "https://serum.trade/",
+    description:"UI Clone of Project Serum Dex",
+    image: "serumtoday.png",
+    tags: ["web"],
+  },
+  {
+    name: "Traderdome",
+    url: "https://traderdome.io/#/",
+    description:"Available in Russian (Русский), Spanish (Español), Korean (한국어), Chinese (简体中文) and Hindi (हिन्दी)",
+    image: "serumtoday.png",
+    tags: ["web","swap","pool"],
   },
 ];
